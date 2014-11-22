77cc40fc0577965a276a924b30d29bbdf0aed6e3
Balázs Suhajda <balazs@onereason.eu>
2014-11-23 00:33:48 +0100

    handling providers and devices in db

--- devices.js
+++ devices.js
@@ -1,19 +1,25 @@
 require( 'es6-promise' ).polyfill();
 var uuid = require('node-uuid');
+var kue = require( 'kue' ),
+	events = kue.createQueue({prefix:'home'});
 
 var db = require( './database-layer.js' );
 
-//var hue = require('./wrappers/hue.js');
-//var lx = require('./wrappers/lifx.js');
 
-
+/* PRIVATE */
 var providers = {}; // should come from redis
 var devices = [];
 
 // TODO: discover new devices
+// TODO: create device tags=> room:kitchen / zone:garden / home:estate
 
 function registerDevice ( device ) {
-	device.id = uuid.v1();
+	devices.push( device );
+}
+
+function registerNewDevice ( device ) {
+	// add home specific id to device
+	device.id = uuid.v4();
 	devices.push( device );
 	db.set( 'devices', devices );
 }
@@ -30,6 +36,10 @@ function registerDevices( deviceList ) {
 	deviceList.filter( filterOutRegisteredDevices ).forEach( registerDevice );
 }
 
+function registerNewDevices( deviceList ) {
+	deviceList.filter( filterOutRegisteredDevices ).forEach( registerNewDevice );
+}
+
 function onRegistrationError ( err ) {
 	console.error( err ); // TODO: error logging
 }
@@ -41,7 +51,7 @@ function registerProviders ( providers ) {
 function registerProvider ( providerName ) {
 	if ( !!providers[ providerName ] ) return;
 	providers[ providerName ] = require( './wrappers/' + providerName + '.js' );
-	providers[ providerName ].getDevices().then( registerDevices ).catch( onRegistrationError );
+	providers[ providerName ].getDevices().then( registerNewDevices ).catch( onRegistrationError );
 }
 
 function registerNewProvider ( provider ) {
@@ -50,9 +60,20 @@ function registerNewProvider ( provider ) {
 }
 
 function init () {
-	db
-		.get( 'providers' )
-		.then( registerProviders );
+	db.get( 'devices' ).then( registerDevices );
+	db.get( 'providers' ).then( registerProviders );
+
+	events.process( 'action', onAction );
 }
+
+
+function onAction( event, done ) {
+	// TODO: untangle devices, tags and standardize statuses
+	var data = event.data;
+
+	console.log( 'onAction', event, data );
+
+}
+
 
 init();
