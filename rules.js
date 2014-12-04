88f1927cbcae9406f81459625f59a2df776d731b
Balázs Suhajda <balazs@onereason.eu>
2014-12-04 21:30:47 +0100

    improved rules logic

--- rules.js
+++ rules.js
@@ -1,10 +1,13 @@
 /**
  * RULES
  */
+
+require( 'es6-promise' ).polyfill();
+var uuid = require( 'node-uuid' );
 var objectAssign = require( 'object-assign' );
 var db = require( './database-layer.js' );
 var kue = require( 'kue' ),
-	events = kue.createQueue({prefix:'home'});
+	events = kue.createQueue({ prefix:'home' });
 
 // TODO: keep system state in db??
 var state = {};
@@ -12,36 +15,75 @@ var state = {};
 var rules = [];
 
 function rightConditions ( conditions, data ) {
-	for ( var key in conditions ) {
-		if ( data[ key ] != conditions[ key ]) return false;
+	for ( var i = 0, condition; condition = conditions[ i ]; i++ ) {
+		for ( var key in condition ) {
+			if ( data[ key ] != condition[ key ]) return false;
+		}
 	}
 	return true;
 }
 
-function takeAction ( action, done, priority ) {
-	console.log( 'takeAction', action );
+function takeAction ( rule, done ) {
+	console.log( 'takeAction', arguments );
 	events.create( 'action', {
-		state: action.state,
-		device: action.device,
-		title: 'Setting ' + action.device + ' to ' + action.state
-	} ).priority( priority ).save();
-	done();
+		deviceSelector: rule.deviceSelector,
+		service       : rule.service,
+		params        : rule.params,
+		title         : 'Setting ' + rule.deviceSelector + ' to ' + rule.service
+	} ).save();
 }
 
 function onEvent( event, done ) {
 	var data = event.data;
 
-	console.log( 'onEvent', event, data );
+	console.log( 'onEvent', data );
 	for ( var i = 0, rule; rule = rules[ i ]; i++ ) {
-		if ( rightConditions( rule.conditions, data )) takeAction( rule.action, done, data._priority );
+		if ( rightConditions( rule.conditions, data )) takeAction( rule );
 	}
 
+	done();
 }
 
-function init() {
+function ready() {
 	events.process( 'event', onEvent );
-	db.get( 'rules' ).then( function ( data ) { rules = data; } ).then( console.log.bind(console));
+	console.log( 'rules.js', 'ready', JSON.stringify( rules, null, "\t"));
+//	takeAction({deviceSelector:'zone:garden', service: 'off'});
+
+
+//	create( [{ source: 'verisure:alarm', status: 'armed' }], 'zone:house', 'off' );
+//	create( [{ source: 'sun-phase', state: 'dusk' }], 'zone:garden', 'on' );
+//	create( [{ source: 'sun-phase', state: 'dawn' }], 'zone:garden', 'off' );
 }
 
+function init () {
+	db.get( 'rules' )
+		.then( function ( data ) { rules = data || []; } )
+		.catch( console.error.bind(console))
+		.then( ready );
+
+}
+
+/* PUBLIC */
+function create ( conditions, deviceSelector, service, params ) {
+	var rule = {
+		conditions    : conditions,
+		deviceSelector: deviceSelector,
+		service       : service,
+		params        : params
+	};
+	rule.id = uuid.v4();
+	rules.push( rule );
+	db.set( 'rules', rules );
+	console.log( 'rules.js', 'new rule create', rule );
+	return rule.id;
+}
+
+function enable ( rule ) {
+	rule.enabled = true;
+}
+
+function disable ( rule ) {
+	rule.enabled = false;
+}
 
 init();
