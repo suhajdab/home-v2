1e348c02e6c3d0e5d0f6743da4480a5c73079fd0
Balázs Suhajda <balazs.suhajda@tretton37.com>
2015-07-28 00:59:17 +0200

    eslint-ing

--- hookshot.js
+++ hookshot.js
@@ -1,9 +1,9 @@
 var hookshot = require( 'hookshot' );
 
-
 hookshot()
-	.on( 'push', function( info ) {
-		console.log( 'hookshot: ref ' + info.ref + ' was pushed.' )
-	});
+	.on( 'push', function onPush( info ) {
+		'use strict';
+		console.log( 'hookshot: ref ' + info.ref + ' was pushed.' );
+	} );
 
 hookshot( 'refs/heads/master', 'git pull' ).listen( 9095 );
--- providers/sunPhases.js
+++ providers/sunPhases.js
@@ -1,13 +1,10 @@
-var CronJob = require( 'cron' ).CronJob;
-var kue = require('kue' ),
-	events = kue.createQueue({prefix:'home'});
+var CronJob = require( 'cron' ).CronJob,
+	kue = require( 'kue' ),
+	suncalc = require( 'suncalc' );
 
 //  calculate sun phases based on day & loc
-var suncalc = require( 'suncalc' );
-
+var events = kue.createQueue( { prefix: 'home' } );
 var geolocation, jobs = [];
-
-
 
 // TODO: subscribe to location update
 
@@ -15,12 +12,14 @@ var geolocation, jobs = [];
  * Function pushes an event to the message queue. Data is passed to it by setting context
  */
 function triggerEvent() {
+	'use strict';
+
 	console.log( 'trigger sun phase event', this );
 	events.create( 'event', {
 		state: this.name,
-		title: phase + ' triggered',
+		title: this.name + ' triggered',
 		source: 'sun-phase'
-	}).save();
+	} ).save();
 }
 
 /**
@@ -30,25 +29,30 @@ function triggerEvent() {
  * @param {Date} event.date - Date object
  */
 function addCronJob( event ) {
+	'use strict';
+
 	console.log( 'adding sun phase cron job', event );
-	jobs.push( new CronJob({
+	jobs.push( new CronJob( {
 		cronTime: event.date,
 		onTick: triggerEvent,
 		start: true,
 		context: event
-	}));
+	} ) );
 }
 
 /**
  * Function loops through sun phases object to create cron jobs
  * @param {Object} events - phase names are keys, phase dates are values
  */
-function generateCronJobs ( events ) {
+function generateCronJobs( events ) {
+	'use strict';
+
 	var now = new Date();
 	for ( var eventName in events ) {
-		if ( events[ eventName ] > now ) addCronJob({ name: eventName, date: events[ eventName ] });
+		if ( events[ eventName ] > now ) {
+			addCronJob( { name: eventName, date: events[ eventName ] } );
+		}
 	}
-
 	// test event 5 sec in the future
 	//addCronJob({ name: 'test event', date: new Date( Date.now() + 5 * 1000 ) });
 }
@@ -56,12 +60,16 @@ function generateCronJobs ( events ) {
 /**
  * Function sets off the generation of cron jobs taking the sun phase objects provided by suncalc
  */
-function setupCronJobs () {
+function setupCronJobs() {
+	'use strict';
+
 	var sunPhases = suncalc.getTimes( Date.now(), geolocation.lat, geolocation.long );
 	generateCronJobs( sunPhases );
 }
 
-function ready () {
+function ready() {
+	'use strict';
+
 	console.log( 'sunPhases ready.' );
 	setupCronJobs();
 }
@@ -69,18 +77,17 @@ function ready () {
 /**
  * Function sets up a repeating cron job to generate sun phase jobs each day, and does generation for current day
  */
-function init ( globalSettings, providerSettings ) {
+function init( globalSettings/*, providerSettings */ ) {
+	'use strict';
+
 	console.log( arguments );
 	geolocation = globalSettings.get( 'geolocation' );
-
 	// cron job to regenerate sun phase crons every day
-	jobs.push( new CronJob({
+	jobs.push( new CronJob( {
 		cronTime: '00 00 00 * * *',
 		onTick: setupCronJobs,
 		start: true
-	}));
-
+	} ) );
 	ready();
 }
-
 module.exports.init = init;
