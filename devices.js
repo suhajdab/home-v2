a0bb1d08e916c21812d955d7c37580b033578db2
Balázs Suhajda <balazs.suhajda@tretton37.com>
2015-07-28 23:34:38 +0200

    linting up devices.js

--- devices.js
+++ devices.js
@@ -1,10 +1,11 @@
 require( 'es6-promise' ).polyfill();
-var uuid = require('node-uuid');
-var kue = require( 'kue' ),
-	events = kue.createQueue( { prefix: 'home' } );
+var uuid = require('node-uuid' ),
+	kue = require( 'kue' );
 
 var db = require( './database-layer.js' );
 var settings = require( './settings.js' );
+
+var events = kue.createQueue( { prefix: 'home' } );
 
 
 /* PRIVATE */
@@ -30,17 +31,27 @@ var devices = [],
  * @param {Array} tags
  */
 function importTags ( tags ) {
+	'use strict';
+
 	tags.forEach( function ( tag ) {
 		var arr = tag.split( ':' );
-		if ( arr.length < 2 ) return;
-		if ( arr[ 0 ] == 'room' && !~rooms.indexOf( arr[ 1 ] )) rooms.push( arr[ 1 ]);
-		if ( arr[ 0 ] == 'zone' && !~zones.indexOf( arr[ 1 ] )) zones.push( arr[ 1 ]);
+		if ( arr.length < 2 ) {
+			return;
+		}
+		if ( arr[ 0 ] === 'room' && !~rooms.indexOf( arr[ 1 ] )) {
+			rooms.push( arr[ 1 ]);
+		}
+		if ( arr[ 0 ] === 'zone' && !~zones.indexOf( arr[ 1 ] )) {
+			zones.push( arr[ 1 ]);
+		}
 	});
 	db.set( 'rooms', rooms );
 	db.set( 'zones', zones );
 }
 
 function findDeviceByTag ( selector ) {
+	'use strict';
+
 	var foundDevices = devices.filter( function ( device ) {
 		return ~( device.tags || [] ).indexOf( selector );
 	});
@@ -50,7 +61,9 @@ function findDeviceByTag ( selector ) {
 }
 
 function findDeviceById( id ) {
-	for ( var i = 0, device; device = devices[ i ]; i++ ) {
+	'use strict';
+
+	for ( var i = 0, device; (device = devices[ i ]); i++ ) {
 		if ( device.id === id ) {
 			console.log( 'found device by id ' + id, device );
 			return device;
@@ -60,47 +73,62 @@ function findDeviceById( id ) {
 }
 
 function deviceSelector ( selector ) {
+	'use strict';
+
 	if ( ~selector.indexOf( ':') ) {
 		return findDeviceByTag( selector );
 	}
-	else return findDeviceById( selector );
+	else {
+		return findDeviceById( selector );
+	}
 }
 
 function registerNewDevice ( device ) {
+	'use strict';
+
 	// add home specific id to device
 	device.id = uuid.v4();
 	devices.push( device );
 	db.set( 'devices', devices );
-	if ( device.tags ) importTags ( device.tags );
+	if ( device.tags ) {
+		importTags ( device.tags );
+	}
 }
 
 function filterOutRegisteredDevices ( device ) {
-	for ( d in devices ) {
-		if ( devices[ d ].nativeId == device.nativeId && devices[ d ].provider == device.provider ) return false;
-	}
+	'use strict';
 
+	for ( var d in devices ) {
+		if ( devices[ d ].nativeId === device.nativeId && devices[ d ].provider === device.provider ) {
+			return false;
+		}
+	}
 	return true;
 }
 
 function registerNewDevices( deviceList ) {
+	'use strict';
+
 	deviceList.filter( filterOutRegisteredDevices ).forEach( registerNewDevice );
 }
 
 function onRegistrationError ( err ) {
+	'use strict';
+
 	console.error( 'onRegistrationError', this, err ); // TODO: error logging
 }
 
-function registerProviders ( providers ) {
-	providers.forEach( registerProvider );
-}
-
 function registerProvider ( providerName ) {
+	'use strict';
+
 	console.log( (new Date()).toTimeString() + 'registering provider: ' + providerName );
 	var currentProvider = platforms[ providerName],
 		providerSettings = settings( providerName ),
 		globalSettings = settings( 'global' );
 
-	if ( currentProvider ) return;
+	if ( currentProvider ) {
+		return;
+	}
 
 	currentProvider = require( './platform/' + providerName + '.js' );
 	currentProvider.init( globalSettings, providerSettings );
@@ -113,12 +141,33 @@ function registerProvider ( providerName ) {
 	platforms[ providerName] = currentProvider;
 }
 
+function registerProviders ( providers ) {
+	'use strict';
+
+	providers.forEach( registerProvider );
+}
+
 function registerNewProvider ( provider ) {
+	'use strict';
+
 	registerProvider( provider );
 	db.set( 'platforms', Object.keys( platforms ));
 }
 
+function onAction( event, done ) {
+	'use strict';
+
+	// TODO: untangle devices, tags and standardize statuses
+	var data = event.data;
+	console.log( 'onAction', data );
+	var device = deviceSelector( data.deviceSelector );
+	platforms[ device.provider ][data.service]( device.nativeId );
+	done();
+}
+
 function ready () {
+	'use strict';
+
 	//console.log( 'device.js ready', devices );
 	events.process( 'action', onAction );
 
@@ -133,6 +182,8 @@ function ready () {
 }
 
 function init () {
+	'use strict';
+
 	Promise.all([
 		db.get( 'platforms' ).then( registerProviders ),
 		db.get( 'devices' ).then( function ( data ) { devices = data; } ),
@@ -141,15 +192,6 @@ function init () {
 	]).then( ready );
 }
 
-
-function onAction( event, done ) {
-	// TODO: untangle devices, tags and standardize statuses
-	var data = event.data;
-	console.log( 'onAction', data );
-	var device = deviceSelector( data.deviceSelector );
-	platforms[ device.provider ][data.service]( device.nativeId );
-	done();
-}
 
 
 init();
