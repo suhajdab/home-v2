365c0b43203cb2b7439c33020edca3383c482696
Balázs Suhajda <balazs.suhajda@tretton37.com>
2015-09-14 14:30:09 +0200

    platform spark beta

--- platform/spark.js
+++ platform/spark.js
@@ -1,23 +1,38 @@
 /*jshint node: true*/
-var Spark = require('spark');
+var spark = require( 'spark' );
 require( 'es6-promise' ).polyfill();
 
 /**
-* Gets a variable value for the device
-* device.getVariable('temp').then()
-*/
+ * Gets a variable value for the device
+ * device.getVariable('temp').then()
+ */
+//TODO: create device object to pass back to devices.js
+//TODO: events to listeners
 
+function onGotEvent() {
+	//console.log( 'Event: ' + JSON.stringify( data ));
+	console.log( arguments );
+}
 
+function onLogin( err, body ) {
+	if ( err ) {
+		console.error( 'spark.js login error:', err );
+	} else {
+		console.log( 'spark.js login successful:', body );
+		//spark.getEventStream( false, 'mine', onGotEvent );
+		spark.listDevices( console.log.bind( console ) );
+	}
+}
 
+function init( globalSettings, platformSettings ) {
+	'use strict';
 
-function init ( globalSettings, platformSettings ) {
-    'use strict';
 	var username = platformSettings.get( 'username' ),
 		password = platformSettings.get( 'password' );
 
-	Spark.login({ username: username, password: password }, function( err, body ) {
-      console.log( 'API call login completed on callback:', body );
-    });
+	spark.login( { username: username, password: password }, onLogin );
 }
 
 module.exports.init = init;
+
+init();
