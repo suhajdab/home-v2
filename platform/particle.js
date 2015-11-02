19e5c5a97f07dbdfe27e7e49b38937352828d1ff
Balázs Suhajda <balazs.suhajda@tretton37.com>
2015-11-02 09:05:45 +0100

    Spark renamed to Particle

--- platform/particle.js
+++ platform/particle.js
@@ -9,6 +9,25 @@ require( 'es6-promise' ).polyfill();
 //TODO: create device object to pass back to devices.js
 //TODO: events to listeners
 
+/**
+ * Function parses response from lifx's cloud api
+ * @param {Object} data  raw json response from api
+ * @returns {Promise}
+ */
+function parseDevices( err, data ) {
+	var newObj = data.map( function ( obj ) {
+		obj = obj.attributes;
+		return {
+			nativeId: obj.id,
+			label: obj.name,
+			type: 'microcontroller',
+			platform: 'particle' // TODO: remove hardcoded platform
+		};
+	} );
+//	return Promise.resolve( newObj );
+	console.log(newObj);
+}
+
 function onGotEvent() {
 	//console.log( 'Event: ' + JSON.stringify( data ));
 	console.log( arguments );
@@ -16,11 +35,11 @@ function onGotEvent() {
 
 function onLogin( err, body ) {
 	if ( err ) {
-		console.error( 'spark.js login error:', err );
+		console.error( 'particle.js login error:', err );
 	} else {
-		console.log( 'spark.js login successful:', body );
-		//spark.getEventStream( false, 'mine', onGotEvent );
-		spark.listDevices( console.log.bind( console ) );
+		console.log( 'particle.js login successful' );
+		spark.getEventStream( false, 'mine', onGotEvent );
+		spark.listDevices( parseDevices );
 	}
 }
 
@@ -34,5 +53,3 @@ function init( globalSettings, platformSettings ) {
 }
 
 module.exports.init = init;
-
-init();
